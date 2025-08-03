3552e9ac870fc2933e934ed11cbb072f00b69890
Agus1783 <17agus03@gmail.com>
2025-08-03 19:56:25 +0700

    mengambil id element id catatan

---
+++ frontend/catatan/catatan.js
@@ -0,0 +1,13 @@
+// url backend
+const url = "http://localhost:3000/catatan";
+
+const inputJudul = document.getElementById("input-judul");
+const inputNote = document.getElementById("input-note");
+const noteBtn = document.getElementById("note-btn");
+const listNote = document.getElementById("list-note");
+
+// fungsi read menampilkan daftar catatan
+
+// fungsi create membuat daftar catatan baru
+
+// fungsi update & delete catatan
