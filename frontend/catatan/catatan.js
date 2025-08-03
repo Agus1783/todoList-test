8f07d35cc3bea9adb036b249ff181ddd3865e374
Agus1783 <17agus03@gmail.com>
2025-08-03 23:09:39 +0700

    membuat fungsi creat read pada catatan.js

--- frontend/catatan/catatan.js
+++ frontend/catatan/catatan.js
@@ -1,3 +1,5 @@
+const { read } = require("fs");
+
 // url backend
 const url = "http://localhost:3000/catatan";
 
@@ -6,8 +8,54 @@ const inputNote = document.getElementById("input-note");
 const noteBtn = document.getElementById("note-btn");
 const listNote = document.getElementById("list-note");
 
-// fungsi read menampilkan daftar catatan
-
 // fungsi create membuat daftar catatan baru
+async function creatNote() {
+    const judul = inputJudul.value.trim();
+    const note = inputNote.value.trim();
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ judul: judul }, {note: note})
+        });
+
+        if (response.ok) {
+            inputJudul.value = '';
+            inputNote.value = '';
+            readNote();
+        };
+    } catch (err) {
+        console.error(`Error tambahkan note: ${err}`)
+    }; 
+};
+
+// fungsi read menampilkan daftar catatan
+async function readNote() {
+    try {
+        const response = await fetch(url);
+        const catatan = await response.json();
+
+        listNote.innerHTML = '';
+
+        catatan.forEach(catatan => {
+            const li = document.createElement('li');
+            li.className = 'list-note-item';
+            li.dataset.no = catatan.no;
+
+            li.innerHTML = `
+                <h3>${catatan.judul}</h3>
+                <span>${catatan.note}</span>
+                <button class="edit-btn">✏️</button>
+                <button class="delete-btn">🗑️</button>
+            `;
+            listNote.appendChild(li);
+        });
+    } catch (err) {
+        console.error(`Error read note: ${err}`);
+    };
+};
 
 // fungsi update & delete catatan
